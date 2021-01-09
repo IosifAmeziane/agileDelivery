47de6d180e6ab2213c7aaeb002fec7cfc2d50c4b
rantofie <rantofie@cisco.com>
2021-01-09 14:42:48 +0200

    Add user controller / security confgiruation

--- src/main/java/sda/projectManagementTool/projectManagement/config/SecurityConfiguration.java
+++ src/main/java/sda/projectManagementTool/projectManagement/config/SecurityConfiguration.java
@@ -8,15 +8,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sda.projectManagementTool.projectManagement.repository.model.UserType;
 import sda.projectManagementTool.projectManagement.service.implementation.SdaUserDetailsService;
 
 @Component
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private SdaUserDetailsService sdaUserDetailsService;
@@ -41,20 +45,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // Role si Authority sunt sinonime - reprezinta acelasi lucru
         // TODO: adaugare restrictii pentru endpoint-urile noi definite
         http.antMatcher("/**")
+                .cors()
+                .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/users").permitAll()
+                .antMatchers("/users/**").permitAll()
                 .antMatchers("/hello-dev").hasAuthority(UserType.DEVELOPER.name()) // Securizam endpointul de hello-dev si-l facem accesibil doar pentru utilizatori cu role de DEVELOPER
-                .antMatchers(HttpMethod.POST, "/projects").hasAuthority(UserType.PROJECT_MANAGER.name()) // Securizam endpointul de hello-pm si-l facem accesibil doar pentru utilizatori cu role de PROJECT_MANAGER
-                .antMatchers(HttpMethod.GET, "/projects/assign-users").hasAuthority(UserType.DEVELOPER.name()) // Securizam endpointul de hello-pm si-l facem accesibil doar pentru utilizatori cu role de PROJECT_MANAGER
+//                .antMatchers(HttpMethod.POST, "/projects").hasAuthority(UserType.PROJECT_MANAGER.name()) // Securizam endpointul de hello-pm si-l facem accesibil doar pentru utilizatori cu role de PROJECT_MANAGER
+//                .antMatchers(HttpMethod.GET, "/projects/assign-users").hasAuthority(UserType.DEVELOPER.name()) // Securizam endpointul de hello-pm si-l facem accesibil doar pentru utilizatori cu role de PROJECT_MANAGER
                 .anyRequest() // precizam ca request-urile anterior configurate trebuie sa fie autorizate (adica trebuie sa avem un user logat)
                 .authenticated()
-                .and()
-                .formLogin() // daca nu, activam formLogin-ul (pagina de Login)
                 .and()
                 .csrf()
                 .disable();
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080");
+            }
+        };
+    }
 
     @Bean
     public PasswordEncoder bCryptPasswordEncoder() {
--- src/main/java/sda/projectManagementTool/projectManagement/controller/UserController.java
+++ src/main/java/sda/projectManagementTool/projectManagement/controller/UserController.java
@@ -1,11 +1,14 @@
 package sda.projectManagementTool.projectManagement.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import sda.projectManagementTool.projectManagement.dto.UserDto;
 import sda.projectManagementTool.projectManagement.repository.model.User;
 import sda.projectManagementTool.projectManagement.service.UserService;
 
 @RestController
+@RequestMapping(path = "/users")
 public class UserController {
 
     private UserService userService;
@@ -14,15 +17,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping(path = "/users")
+    @PostMapping
     public User save(@RequestBody UserDto userDto) {
         User user = userService.mapUserDtoToUser(userDto);
         return userService.save(user);
     }
 
-    @GetMapping(path = "/users/{id}")
+    @GetMapping(path = "/{id}")
     public User findById(@PathVariable Long id) {
         return userService.findById(id);
     }
 
+    @GetMapping(path = "/logged-user")
+    public HttpStatus getLoggedUser() {
+        return HttpStatus.OK;
+    }
 }
